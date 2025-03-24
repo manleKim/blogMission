e537fe97cce0317a36880c5a1c553bdf3963dffc
manleKim <kk410910@naver.com>
2025-03-24 15:37:46 +0900

    Init: 프로젝트 초기화

---
+++ src/test/java/com/sprint/mission/blog/BlogMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogMissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
